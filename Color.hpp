752318e2e73ed61398c5d721822ecc89cae7748d
asdfer1234 <sh__lv@naver.com>
2022-11-12 18:18:49 +0900

    Add Color

---
+++ Color.hpp
@@ -0,0 +1,21 @@
+#pragma once
+enum class Color
+{
+	Black,
+	DarkBlue,
+	DarkGreen,
+	Aqua,
+	DarkRed,
+	Purple,
+	DarkYellow,
+	LightGray,
+	Gray,
+	Blue,
+	Green,
+	Cyan,
+	Red,
+	Purple,
+	Yellow,
+	White
+};
+
