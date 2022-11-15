4e2159ee40d57aed946f6b29548d2efa312110ff
asdfer1234 <sh__lv@naver.com>
2022-11-15 21:37:06 +0900

    Implement Pixel !=

--- Pixel.cpp
+++ Pixel.cpp
@@ -5,3 +5,10 @@ Pixel::Pixel() : Pixel(' ') {}
 Pixel::Pixel(char character) : Pixel(character, Palette::basic){}
 
 Pixel::Pixel(char character, Palette palette) : character(character), palette(palette) {}
+
+bool operator==(const Pixel &a, const Pixel &b){
+	return a.character == b.character && a.palette == b.palette;
+}
+bool operator!=(const Pixel &a, const Pixel &b){
+	return !(a == b);
+}
